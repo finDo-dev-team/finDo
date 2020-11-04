805d90afedf1a550e616001c258dd29150b00f68
jestin-g <jestingabriel@gmail.com>
2020-11-04 10:33:23 +0100

    ajout faux commentaire

--- database/migrations/2020_10_12_165409_create_events_table.php
+++ database/migrations/2020_10_12_165409_create_events_table.php
@@ -14,7 +14,7 @@ class CreateEventsTable extends Migration
     public function up()
     {
         Schema::create('events', function (Blueprint $table) {
-            $table->id();
+            $table->id(); /* id */
             $table->string('title');
             $table->date('date');
             $table->text('location');
