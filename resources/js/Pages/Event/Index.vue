505d9dd149612d2525ccd911e6c09eefefca6e3e
jestin-g <jestingabriel@gmail.com>
2020-11-13 20:12:59 +0100

    Amélioration affichage des événements

--- resources/js/Pages/Event/Index.vue
+++ resources/js/Pages/Event/Index.vue
@@ -5,19 +5,24 @@
         Vue des events
       </h1>
     </template>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:px-1/12">
+    <div class="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3 lg:px-1/12">
       <div v-for="event in this.eventList" v-bind:key="event.id">
-        <div class="rounded overflow-hidden shadow-lg h-full">
-          <div class="px-6 py-4">
+        <div class="rounded overflow-hidden shadow h-full bg-white relative hover:shadow-2xl duration-500 ease-in-out">
+          <div class="px-6 py-4 mb-16">
             <p class="text-gray-600 text-xs">
               {{ event.date }}
             </p>
+            <div v-for="type in event.type" v-bind:key="type.id">
+              <p class="text-gray-600 text-xs">
+                {{ type.label }}
+              </p>
+            </div>
             <div class="font-bold text-xl mb-2">{{ event.title }}</div>
             <p class="text-gray-700 text-base">
               {{ event.description }}
             </p>
           </div>
-          <div class="px-6 pt-4 pb-2">
+          <div class="px-6 pb-2 absolute bottom-0">
             <span
               class="inline-block bg-gray-100 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2"
               >{{ event.location }}</span
