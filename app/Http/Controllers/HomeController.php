f24e75b56f716fd9d35651b064a9c7e615928374
jestin-g <jestingabriel@gmail.com>
2020-11-14 14:15:09 +0100

    renvoi bien les 3 prochains événements à venir

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $events = Event::orderBy('date', 'asc')->get()->take(3);
+        $events = Event::orderBy('date', 'asc')
+            ->whereDate('date', '>=', date('Y-m-d'))
+            ->get()
+            ->take(3);
+        dd($events);
         return Inertia::render('Home', [
             'events' => $events
         ]);
@@ -20,6 +23,4 @@ class HomeController extends Controller
     {
         return Inertia::render('Map');
     }
-
-
 }
