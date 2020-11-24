8942c961aaa577b377118b199b9effd39b8a296c
jestin-g <jestingabriel@gmail.com>
2020-11-24 10:19:03 +0100

    Creation des composants EventListItem et EventList

---
+++ resources/js/Pages/Components/EventListComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3 lg:px-1/12">
+    <EventListItem
+      v-for="event in this.events"
+      v-bind:key="event.id"
+      :event="event"
+    ></EventListItem>
+  </div>
+</template>
+
+<script>
+import EventListItem from "./EventListItemComponent";
+
+export default {
+  components: {
+    EventListItem,
+  },
+
+  name: "EventList",
+  props: ["events"],
+};
+</script>
