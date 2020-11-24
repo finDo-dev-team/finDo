ccf3f5b8f943584af63271c4caff7a60937a3928
jestin-g <jestingabriel@gmail.com>
2020-11-24 11:57:39 +0100

    ajout des boutons de tri

--- resources/js/Pages/Components/EventListComponent.vue
+++ resources/js/Pages/Components/EventListComponent.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3 lg:px-1/12">
-    <EventListItem
-      v-for="event in this.events"
-      v-bind:key="event.id"
-      :event="event"
-    ></EventListItem>
+  <div>
+    <button v-on:click="orderDateAsc">Trier par ordre croissant</button>
+    <button v-on:click="orderDateDesc">Trier par ordre décroissant</button>
+
+    <div
+      class="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3 lg:px-1/12"
+    >
+      <EventListItem
+        v-for="event in this.events"
+        v-bind:key="event.id"
+        :event="event"
+      ></EventListItem>
+    </div>
   </div>
 </template>
 
@@ -12,11 +19,24 @@
 import EventListItem from "./EventListItemComponent";
 
 export default {
+  name: "EventList",
+  props: ["events"],
+
   components: {
     EventListItem,
   },
 
-  name: "EventList",
-  props: ["events"],
+  methods: {
+    orderDateAsc: function () {
+      this.events.sort(function (a, b) {
+        return a.date > b.date;
+      });
+    },
+    orderDateDesc: function () {
+      this.events.sort(function (a, b) {
+        return a.date < b.date;
+      });
+    },
+  },
 };
 </script>
