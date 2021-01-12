ab229430e5a86a29bcb8b52626f1db5f4c0c529a
jestin-g <jestingabriel@gmail.com>
2021-01-12 16:21:42 +0100

    corrcetion commment

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,11 +9,6 @@ class Event extends Model
 {
     use HasFactory;
 
-    /**
-     * Définit la relation "Many To Many" entre Event et Type.
-     * 
-     * @return Illuminate\Database\Eloquent\Concerns\HasRelationships::belongsToMany
-     */
     protected $fillable = [
         'title',
         'location',
@@ -21,6 +16,11 @@ class Event extends Model
         'description'
     ];
 
+    /**
+     * Définit la relation "Many To Many" entre Event et Type.
+     * 
+     * @return Illuminate\Database\Eloquent\Concerns\HasRelationships::belongsToMany
+     */
     public function types()
     {
         return $this->belongsToMany('App\Models\EventType');
