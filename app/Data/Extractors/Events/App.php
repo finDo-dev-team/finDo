0df53c20096403cbfaca52dd3eff856130582cf5
Aniss Elarj <aniss.elarj92@gmail.com>
2021-02-14 17:15:15 +0100

    Ajout api OpenData Ile-de-France

--- app/Data/Extractors/Events/App.php
+++ app/Data/Extractors/Events/App.php
@@ -3,6 +3,7 @@ namespace App\Data\Extractors\Events;
 
 use App\Data\Extractors\Events\ODPExtractor;
 use App\Data\Extractors\Events\ODSExtractor;
+use App\Data\Extractors\Events\ODIDFExtractor;
 
 class App {
     public static function main(): void
@@ -12,5 +13,8 @@ class App {
 
         $odsExtractor = new ODSExtractor();
         $odsExtractor->addEventsToDB();
+
+        $odidfExtractor = new ODIDFExtractor();
+        $odidfExtractor->addEventsToDB();
     }
 }
