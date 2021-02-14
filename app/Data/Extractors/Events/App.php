eea2a029209989ccf8de42abb3c1015141077068
Aniss Elarj <aniss.elarj92@gmail.com>
2021-02-14 16:48:34 +0100

    Ajout api OpendataSoft + Modif affichage description

--- app/Data/Extractors/Events/App.php
+++ app/Data/Extractors/Events/App.php
@@ -2,11 +2,15 @@
 namespace App\Data\Extractors\Events;
 
 use App\Data\Extractors\Events\ODPExtractor;
+use App\Data\Extractors\Events\ODSExtractor;
 
 class App {
     public static function main(): void
     {
         $odpExtractor = new ODPExtractor();
         $odpExtractor->addEventsToDB();
+
+        $odsExtractor = new ODSExtractor();
+        $odsExtractor->addEventsToDB();
     }
 }
