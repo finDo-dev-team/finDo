ded55b6f52d314a75a782ec5bfe69f1aa1e20797
Aniss Elarj <aniss.elarj92@gmail.com>
2021-03-07 20:50:41 +0100

    Ajout Open data Lille

--- app/Data/Extractors/Events/App.php
+++ app/Data/Extractors/Events/App.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Data\Extractors\Events;
 
+use App\Data\Extractors\Events\ODLExtractor;
 use App\Data\Extractors\Events\ODPExtractor;
 use App\Data\Extractors\Events\ODSExtractor;
-use App\Data\Extractors\Events\ODIDFExtractor;
 use App\Data\Extractors\Events\ODGPSExtractor;
+use App\Data\Extractors\Events\ODIDFExtractor;
+
 class App {
     public static function main(): void
     {
@@ -20,5 +22,9 @@ class App {
 
         $odgpsExtractor = new ODGPSExtractor();
         $odgpsExtractor->addEventsToDB();
+
+        $odlExtractor = new ODLExtractor();
+        $odlExtractor->addEventsToDB();
+    
     }
 }
