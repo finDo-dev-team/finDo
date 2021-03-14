9a57090ae19ba77d3c22d582de71b2721d7ab6db
CLebien <nd.clebien>
2021-03-14 02:45:18 +0100

    Codage du composant ShareEventComponent

---
+++ resources/js/Pages/Components/ShareEventComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div v-for="event in events" v-bind:key="event.id">
+            <a @click.prevent="shareFacebook(event.url, event.title)">Share on Facebook</a>
+            <a @click.prevent="shareTweeter(event.url, event.title)">Share on Twitter</a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        name: "ShareEvent",
+        props: ["event"],
+
+        data() {
+            return {
+                providers: {
+                    twitter: 'https://twitter.com/intent/tweet/?url=:url&text=:text',
+                    facebook: 'https://www.facebook.com/sharer/sharer.php?u=:u&title=:title'
+                }
+            };
+        },
+
+        methods: {
+
+            shareFacebook(url,title) {
+                window.open(this.providers.facebook.replace(':u', url).replace(':title', title), 'sharer', 'toolbar=0,status=0,width=648,height=395');
+                return true;
+            },
+
+            shareTweeter(url,title) {
+                window.open(this.providers.twitter.replace(':u', url).replace(':title', tilte), 'sharer', 'toolbar=0,status=0,width=648,height=395');
+                return true;
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
