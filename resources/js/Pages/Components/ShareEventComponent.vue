1a04bce5007d969eb8e8cd8734b3caa17a68baaf
CLebien <nd.clebien>
2021-03-25 02:39:29 +0100

    change cursor style

--- resources/js/Pages/Components/ShareEventComponent.vue
+++ resources/js/Pages/Components/ShareEventComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <a :href="'/download/' + event.id" class="btn-space"><i class="twitter far fa-calendar-alt"></i></i></a>
-        <button  class=" btn-space" @click.prevent="shareTweeter()"><i class=" twitter fab fa-twitter"></i></button>
-        <button class="btn-space" @click.prevent="shareFacebook()"><i class="twitter fab fa-facebook"></i></button>
-        <button class="btn-space " @click.prevent="shareLinkedin()"><i class="twitter fab fa-linkedin"></i></button>
+        <a :href="'/download/' + event.id" class="btn-space"><i class="icon-color far fa-calendar-alt"></i></a>
+        <a  class=" btn-space pointer" @click.prevent="shareTweeter()"><i class=" icon-color fab fa-twitter"></i></a>
+        <a class="btn-space pointer" @click.prevent="shareFacebook()"><i class="icon-color fab fa-facebook"></i></a>
+        <a class="btn-space pointer" @click.prevent="shareLinkedin()"><i class="icon-color fab fa-linkedin"></i></a>
     </div>
 </template>
 
@@ -22,9 +22,10 @@ import '@fortawesome/fontawesome-free/js/all.js';
             return {
                 url: 'http%3A%2F%2Flocalhost%3A8000/DetailledEvent/'+this.event.id,
                 title: this.event.lead_text,
+                tag: this.event.types ,
                 providers: {
                     facebook: 'https://www.facebook.com/sharer.php?u=:u&title=:title',
-                    twitter: 'https://twitter.com/intent/tweet?url=:u&title=:title',
+                    twitter: 'https://twitter.com/intent/tweet?url=:u&title=:title&hashtags=:tag',
                     linkedin: 'https://www.linkedin.com/shareArticle?url=:u&title=:title'
                 }
             };
@@ -38,7 +39,7 @@ import '@fortawesome/fontawesome-free/js/all.js';
             },
 
             shareTweeter() {
-                window.open(this.providers.twitter.replace(':u', this.url).replace(':title', this.title), 'sharer', 'toolbar=0,status=0,width=648,height=395');
+                window.open(this.providers.twitter.replace(':u', this.url).replace(':title', this.title).replace(':tag', this.tag), 'sharer', 'toolbar=0,status=0,width=648,height=395');
                 return true;
             },
 
@@ -55,8 +56,14 @@ import '@fortawesome/fontawesome-free/js/all.js';
         margin: 8px;
     }
 
-    .twitter{
+    .icon-color{
         color: #00c3ff;
+    }
+    .pointer{
+        cursor: pointer;
+    }
+    .pointer:active{
+        cursor: progress;
     }
 
 </style>
