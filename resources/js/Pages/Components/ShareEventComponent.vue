6f8fa464bac1aa7c9ce400b1814ffc1c26de2bbe
CLebien <nd.clebien>
2021-03-27 16:30:52 +0100

    Refactor shareEventComponent

--- resources/js/Pages/Components/ShareEventComponent.vue
+++ resources/js/Pages/Components/ShareEventComponent.vue
@@ -3,7 +3,6 @@
         <a :href="'/download/' + event.id" class="btn-space"><i class="icon-color far fa-calendar-alt"></i></a>
         <a  class=" btn-space pointer" @click.prevent="shareTweeter()"><i class=" icon-color fab fa-twitter"></i></a>
         <a class="btn-space pointer" @click.prevent="shareFacebook()"><i class="icon-color fab fa-facebook"></i></a>
-        <a class="btn-space pointer" @click.prevent="shareLinkedin()"><i class="icon-color fab fa-linkedin"></i></a>
     </div>
 </template>
 
@@ -20,13 +19,11 @@ import '@fortawesome/fontawesome-free/js/all.js';
 
         data() {
             return {
-                url: 'http%3A%2F%2Flocalhost%3A8000/DetailledEvent/'+this.event.id,
+                url: window.location.host+'%2FDetailledEvent%2F'+this.event.id,
                 title: this.event.lead_text,
-                tag: this.event.types ,
                 providers: {
-                    facebook: 'https://www.facebook.com/sharer.php?u=:u&title=:title',
-                    twitter: 'https://twitter.com/intent/tweet?url=:u&title=:title&hashtags=:tag',
-                    linkedin: 'https://www.linkedin.com/shareArticle?url=:u&title=:title'
+                    facebook: 'https://www.facebook.com/sharer.php?u=:u&title=:text',
+                    twitter: 'https://twitter.com/intent/tweet?url=:u&title=:text'
                 }
             };
         },
@@ -34,20 +31,17 @@ import '@fortawesome/fontawesome-free/js/all.js';
         methods: {
 
             shareFacebook() {
-                window.open(this.providers.facebook.replace(':u', this.url).replace(':title', this.title), 'sharer', 'toolbar=0,status=0,width=648,height=395');
+                window.open(this.providers.facebook.replace(':u', this.url).replace(':text', this.title), 'sharer', 'toolbar=0,status=0,width=648,height=395');
                 return true;
             },
 
             shareTweeter() {
-                window.open(this.providers.twitter.replace(':u', this.url).replace(':title', this.title).replace(':tag', this.tag), 'sharer', 'toolbar=0,status=0,width=648,height=395');
+                window.open(this.providers.twitter.replace(':u', this.url).replace(':text', this.title).replace(':tag', this.tag), 'sharer', 'toolbar=0,status=0,width=648,height=395');
                 return true;
             },
 
-            shareLinkedin() {
-                window.open(this.providers.linkedin.replace(':u', this.url).replace(':title', this.title), 'sharer', 'toolbar=0,status=0,width=648,height=395');
-                return true;
-            }
         },
+
     }
 </script>
 
